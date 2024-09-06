1799591be021bb2ec513de941d983436ed290557
raditya12 <radityariefki5@gmail.com>
2024-09-06 10:11:10 +0700

    week1

---
+++ Latihan23.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan23
+ */
+public class Latihan23 {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Nama     : Raditya Riefki");
+        System.out.println("NIM      : 244107020204");
+        System.out.println("Kelas    : TI-1E");
+        System.out.println("No Absen : 23");
+        System.out.println("Prodi    : D IV Teknik Informatika");
+        System.out.println("Alamat   : Jl Kh Hasyim Asyari No 160");
+        System.out.println("No HP    : 081357462594");
+        System.out.println("Hobi     : Basket");
+    }
+}
